194b34020a0ef9c12272f3f8f9e1f6eb91728534
ramyavarthini <ramyavarthini@gmail.com>
2018-12-04 12:15:41 +0530

    Added new files

---
+++ areacircumdiame_circle.py
@@ -0,0 +1,31 @@
+
+print('*********CIRCLE***********')
+pi = 3.14
+print('Type ofpi value:',type(pi))
+radius = int(input('Enter radius:'))
+
+area = pi*radius*radius
+print('Area of Circle:',area)
+
+circumference = 2*pi*radius
+print('Circumference of circle:',circumference)
+
+diameter = 2*radius
+print('Diameter of Cirlce:',diameter)
+print('*********CIRCLE***********',end='\n')
+
+print('*********TRIANGLE***********')
+
+base = int(input('Enter base:'))
+height = int(input('Enter height:'))
+area_triangle = (base*height)/2
+print('Area of Triangle:',area_triangle)
+
+print('*********TRIANGLE***********',end='\n\n\n')
+
+print('*********RECTANGLE***********')
+
+area_rect = base*height
+print('Area of Rectangle:', area_rect)
+
+print('*********RECTANGLE***********')
---
+++ if_pgm2.py
@@ -0,0 +1,5 @@
+number = int(input('Enter number'))
+if number>=1 and number<=10:
+    print('The number exists in between 0 and 10')
+else:
+    print('The number does exist in between 0 and 10')
