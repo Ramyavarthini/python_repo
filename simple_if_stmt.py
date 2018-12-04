b8735c4c341fee1dd4ea1d8f4e23ae0a7fbac41c
ramyavarthini <ramyavarthini@gmail.com>
2018-12-04 13:03:04 +0530

    test file updated

--- simple_if_stmt.py
+++ simple_if_stmt.py
@@ -2,4 +2,4 @@ age = int(input('Enter Age:'))
 if age >= 18:
 	print('You are eligible for vote')
 else:
-        print('You are not eligible for votes')
+        print('You are not eligible for vote')
